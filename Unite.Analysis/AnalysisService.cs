771342240ad191cf78e51a8c28c8ab9d1047ed55
vulasau <valiantsin.ulasau@gmail.com>
2023-12-21 09:40:15 +0100

    Update AnalysisService.cs

--- Unite.Analysis/AnalysisService.cs
+++ Unite.Analysis/AnalysisService.cs
@@ -15,37 +15,29 @@ public abstract class AnalysisService<TModel, TResult> where TModel : class
     {
         var stopwatch = new Stopwatch();
         var httpClientHandler = new HttpClientHandler() { UseProxy = false };
-        var httpClient = new HttpClient(httpClientHandler) { Timeout = TimeSpan.FromMinutes(60) };
+        using var httpClient = new HttpClient(httpClientHandler) { Timeout = TimeSpan.FromMinutes(60) };
 
-        try
+        stopwatch.Start();
+
+        var request = new HttpRequestMessage(HttpMethod.Post, url);
+        var response = await httpClient.SendAsync(request);
+
+        stopwatch.Stop();
+
+        if (response.IsSuccessStatusCode)
         {
-            stopwatch.Start();
-
-            var request = new HttpRequestMessage(HttpMethod.Post, url);
-            var response = await httpClient.SendAsync(request);
-
-            stopwatch.Stop();
-
-            if (response.IsSuccessStatusCode)
-            {
-                return AnalysisTaskResult.Success(stopwatch.Elapsed.TotalSeconds);
-            }
-            else
-            {
-                var statusCode = (int)response.StatusCode;
-
-                if (statusCode == 501)
-                    return AnalysisTaskResult.Rejected();
-                else if (statusCode == 500)
-                    return AnalysisTaskResult.Failed();
-                else 
-                    throw new NotImplementedException();
-            }
+            return AnalysisTaskResult.Success(stopwatch.Elapsed.TotalSeconds);
         }
-        finally
+        else
         {
-            stopwatch.Stop();
-            httpClient.Dispose();
+            var statusCode = (int)response.StatusCode;
+
+            if (statusCode == 501)
+                return AnalysisTaskResult.Rejected();
+            else if (statusCode == 500)
+                return AnalysisTaskResult.Failed();
+            else 
+                throw new NotImplementedException();
         }
     }
 }
